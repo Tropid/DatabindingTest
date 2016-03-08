7bf0ee71e51a9776e4406f8a6d0e3e24e7c5b192
Richard Keller <rdk.keller@gmail.com>
2016-03-09 00:19:08 +0100

    Remove unused logger and validate method in model

--- src/de/rdk/adressbuch/Model.java
+++ src/de/rdk/adressbuch/Model.java
@@ -1,15 +1,6 @@
 package de.rdk.adressbuch;
 
-import com.google.common.collect.ImmutableList;
-import de.rdk.databinding.ObservableString;
-import de.rdk.validation.StringValidator;
-import de.rdk.validation.Validator;
-import org.apache.log4j.Logger;
-
 public final class Model {
-    
-    private static final Logger LOGGER = Logger.getLogger(Model.class);
-    
     private final Person person;
     
     public Model() {
@@ -18,17 +9,5 @@ public final class Model {
     
     public Person person() {
         return this.person;
-    }
-    
-    public boolean validate() {
-        StringValidator nameValidator = new StringValidator();
-        nameValidator.maxLength(10);
-        nameValidator.minLength(3);
-        
-        ImmutableList<Validator.Violation> violations = nameValidator.validate(person().name().get());
-        for (Validator.Violation violation : violations) {
-            LOGGER.info(String.format("Constraint Violation: %s", violation.message()));
-        }
-        return violations.isEmpty();
     }
 }
